1645b49e9a5c14b565816a308cc2fb4ea3f651d0
Yuta <ymmt7974@gmail.com>
2020-09-13 03:25:33 +0900

    コールバックの実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,22 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
   private
 
+  # [before]
+  # 名称：空白の場合、「（名前なし）」を設定
+  def set_nameless_name
+    self.name = '（名前なし）' if name.blank?
+  end
+
+  # 名称：カンマチェック
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマ(,)を含めることはできません') if name&.include?(',')
   end
+
+
 end
